5e1c8b41403c1e6502ca2bb859e21ac875fb9769
haphuongdien <dien.haphuong0526@hcmut.edu.vn>
2023-08-12 22:07:43 -0700

    Add function to convert string to dateime

--- utils/datetime.py
+++ utils/datetime.py
@@ -1,7 +1,17 @@
 from datetime import datetime
 
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
 def parse_date_string(date_string):
     return datetime.fromisoformat(date_string.replace('Z', '+00:00'))
 
 def format_date_string(date_obj):
-    return date_obj.strftime('%Y-%m-%d %H:%M:%S')
+    return date_obj.strftime(DATE_FORMAT)
+
+def convert_string_to_datetime(date_string):
+    try:
+        datetime_object = datetime.strptime(date_string, DATE_FORMAT)
+        return datetime_object
+    except ValueError:
+        print("Error: Unable to parse the date string.")
+        return None
